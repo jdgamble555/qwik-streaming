1211291e4029e2e7e9dcfc2d0c182bd1b19e4df8
Jonathan Gamble <jdgamble555@gmail.com>
2025-04-04 19:51:02 -0500

    first

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,35 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, Resource } from "@builder.io/qwik";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+
+
+type Todo = {
+  title: string
+};
+
+export const useTodo = routeLoader$(() => {
+  return async () => {
+    const randomTodo = Math.floor(Math.random() * 200) + 1;
+    return await fetch(`https://jsonplaceholder.typicode.com/todos/${randomTodo}`)
+      .then(r => r.json()) as Todo;
+  }
+});
+
 
 export default component$(() => {
+
+  const todo = useTodo();
+
+  // NOTE: onPending does not work (yet) in Qwik
+  // https://github.com/QwikDev/qwik/issues/4328
+
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
+    <main class="flex flex-col justify-center items-center mt-5 gap-3">
+      <h1 class="text-2xl">Todo</h1>
+      <Resource
+        value={todo}
+        onResolved={(todo) => <h2>{todo.title}</h2>}
+      />
+    </main>
   );
 });
 
